2fc1840680e0b0a7191f02c0d0beddfb0ae255cb
Royyan <mproyyan@gmail.com>
2026-05-19 09:01:01 +0700

    create introduction page

---
+++ ConarLyze/Extensions/ColorExtension.swift
@@ -0,0 +1,15 @@
+//
+//  ColorExtension.swift
+//  ConarLyze
+//
+//  Created by Muhammad Pandu Royyan on 19/05/26.
+//
+
+import SwiftUI
+
+extension Color {
+  static let primaryColor = Color("PrimaryColor")
+  static let secondaryColor = Color("SecondaryColor")
+  static let tertiaryColor = Color("TertiaryColor")
+  static let quarternaryColor = Color("QuartenaryColor")
+}

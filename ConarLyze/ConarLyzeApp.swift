6f80d5ac055cad920c6d878b3d1252e5f04d4681
Royyan <mproyyan@gmail.com>
2026-05-19 08:13:54 +0700

    Initial Commit

---
+++ ConarLyze/ConarLyzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConarLyzeApp.swift
+//  ConarLyze
+//
+//  Created by Muhammad Pandu Royyan on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ConarLyzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConarLyze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConarLyze
+//
+//  Created by Muhammad Pandu Royyan on 19/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

204bf76659f55c1b35a492f0bd2678e9f3c54ad9
daffabur <daffaburane123@gmail.com>
2026-05-25 11:11:25 +0700

    task 2.2 - 2.3

--- ConarLyze/App/DIContainer.swift
+++ ConarLyze/App/DIContainer.swift
@@ -16,18 +16,36 @@ final class DIContainer: ObservableObject {
     let apiClient: APIClient
     let localStateRepository: LocalStateRepositoryProtocol
     
+    // MARK: - Feature Repositories
+    
+    let analysisRepository: AnalysisRepositoryProtocol
+    let outfitRepository: OutfitRepositoryProtocol
+    
     // MARK: - Init
     
     init() {
         self.apiClient = APIClient.shared
         self.localStateRepository = LocalStateRepository.shared
+        
+        self.analysisRepository = AnalysisRepository(
+            apiClient: APIClient.shared,
+            localStateRepository: LocalStateRepository.shared
+        )
+        
+        self.outfitRepository = OutfitRepository(
+            apiClient: APIClient.shared
+        )
     }
     
     init(
         apiClient: APIClient,
-        localStateRepository: LocalStateRepositoryProtocol
+        localStateRepository: LocalStateRepositoryProtocol,
+        analysisRepository: AnalysisRepositoryProtocol,
+        outfitRepository: OutfitRepositoryProtocol
     ) {
         self.apiClient = apiClient
         self.localStateRepository = localStateRepository
+        self.analysisRepository = analysisRepository
+        self.outfitRepository = outfitRepository
     }
 }
